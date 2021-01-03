d134e011f5b47b5a2937112a144cfa3237b053c1
Djellze <djellzebllaca@gmail.com>
2021-01-04 00:20:37 +0100

    Delete unused methods

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -19,16 +19,6 @@ class SaleController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -72,28 +62,6 @@ class SaleController extends Controller
                 ];
             }
         return response()->json($output, $code);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
